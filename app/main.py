37d17f14c4feb8cd60e20822ed63281c67128d40
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2024-11-05 17:54:52 +0100

    Parameters changed

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from fastapi import Query
 app = FastAPI()
 
 @app.get("/")
-async def root(thisfilter: str = Query(None)):
-    q = '"coin/coin-related"@en'
-    return Response(json.dumps(buildgraph(q, thisfilter), indent=4), media_type="application/json", headers={"Access-Control-Allow-Origin": "*"})
+async def root(q: str = Query(None)):
+    thistopic = '"coin/coin-related"@en'
+    return Response(json.dumps(buildgraph(thistopic, q), indent=4), media_type="application/json", headers={"Access-Control-Allow-Origin": "*"})
     #return {"message": "Hello, FastAPI with Docker!"}
 
